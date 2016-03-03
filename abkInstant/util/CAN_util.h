ef9515f5af7d2646f26a0d76275e8bca8d803844
root <root@rpiPlus.(none)>
2016-03-03 13:31:14 -0500

    removed junk files

--- abkInstant/util/CAN_util.h
+++
@@ -1,9 +0,0 @@
-
-
-
-byte extract_CAN_msg( struct sCAN* Msg, byte* DataPack			  );
-int  pack_CAN_msg   ( struct sCAN* msg, byte* buffer, int bufSize );
-void can_interface  ( );
-
-void dump_buffer(byte* buffer, int bufSize);
-
